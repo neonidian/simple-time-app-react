60fabbda1c56ca3e215fca323a4cfa26b7bfd59f
Neonidian <neonidian@gmail.com>
2020-05-26 18:08:50 +0200

    Refactor HomePage.js

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,21 +1,22 @@
 import React, {useState, useEffect} from 'react';
 
 const HomePage = () => {
-    let [time, setTime] = useState('Loading');
+    let [time, setTime] = useState({datetime: 'Loading'});
 
     useEffect(() => {
             const fetchTime = fetch('http://worldtimeapi.org/api/ip')
-                .then(response => response.json())
-                .then(response => JSON.stringify(response));
+                .then(response => response.json());
             fetchTime.then(data => setTime(time = data));
         }, []
     );
 
+    let toDisplay = time.datetime === 'Loading' ? 'Loading...' :
+        <p>Time based on IP address: <strong>{new Date(time.datetime).toTimeString()}</strong></p>;
+
     return (
         <main>
-            {time}
+            {toDisplay}
         </main>);
 };
-
 
 export default HomePage;
