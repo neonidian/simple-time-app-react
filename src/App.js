d3738134e4b9833867bcb6582a406376d85f9f1a
neonidian <neonidian@gmail.com>
2020-05-28 18:37:36 +0200

    Add footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import HomePage from './components/Home/HomePage';
+import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
         <>
             <Header />
             <HomePage />
+            <Footer />
         </>
     );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            API used from <em>worldtimeapi.org</em>
+        </footer>
+    );
+};
+
+export default Footer;
