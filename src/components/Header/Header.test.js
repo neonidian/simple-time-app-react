3ecd5f8e7aeb626809741de221cada1b2adb2e93
Keerthi Shankar Subramanian <keerthi.subramanian@cepheid.com>
2020-12-28 10:53:59 +0100

    Fix tests

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {render} from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
 import Header from './Header';
 
 test('renders header with <h1> tag with "time" text', () => {
--- src/components/TimeBasedOnIpAddress/TimeBasedOnIpAddress.test.js
+++ src/components/TimeBasedOnIpAddress/TimeBasedOnIpAddress.test.js
@@ -5,5 +5,5 @@ import TimeBasedOnIpAddress from './TimeBasedOnIpAddress';
 test('renders a section of the page with "Loading..." text when the page is first mounted', () => {
     const {getByTestId} = render(<TimeBasedOnIpAddress />);
     const headerElement = getByTestId('time-ip-address');
-    expect(headerElement).toHaveTextContent('Loading...')
+    expect(headerElement.textContent).toBe('Loading...')
 });
