ceea1d13011da8ec7954a434fc4a894b471e10f7
Keerthi Shankar Subramanian <keerthi.subramanian@mitigram.com>
2021-03-07 21:05:45 +0100

    Fix build step - 'npm install' - use required import

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 version = "2020.2"
