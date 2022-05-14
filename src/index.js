2dcdca21d15d59d2fcecb1dba0ff391ca8818bd9
neonidian <neonidian@gmail.com>
2022-05-14 19:36:48 +0530

    Upgrade packages, react to ver.18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-    <StrictMode>
-        <App/>
-    </StrictMode>,
-    document.getElementById('root')
-);
+const root = createRoot(document.getElementById('root'));
+root.render(<App/>);
